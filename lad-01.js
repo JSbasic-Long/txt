3a5618007a0c3b9ac8fe60cb82a31a373b3e5821
JSbasic-Long <hoanglongngl98@gmai.com>
2023-09-20 19:44:17 +0700

    aa

---
+++ lad-01.js
@@ -0,0 +1,26 @@
+// Bài 1
+// Câu a
+console.log('Hello World')
+// Câu b
+console.log('Nguyễn Lê Hoàng Long')
+console.clear()
+// Câu d
+console.log('*****')
+console.log('*****')
+console.log('*****')
+console.log('*****')
+console.log('*****')
+// Câu e
+console.log('     *')
+console.log('    ***')
+console.log('   *****')
+console.log('  *******')
+console.log(' **********')
+console.log('************')
+// Câu f
+console.log(' ** ** ')
+console.log('*  *  *')
+console.log('*     *')
+console.log(' *   *')
+console.log('  * *')
+console.log('   *')
