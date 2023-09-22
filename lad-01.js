47ed22fcdda1af93b7cdf92c4cac0c441d1d16b6
JSbasic-Long <hoanglongngl98@gmai.com>
2023-09-22 19:31:43 +0700

    22/9/2023

--- lad-01.js
+++ lad-01.js
@@ -1,37 +1,85 @@
-// Bài 1 :
-// Câu a :
-console.log('Hello World')
-// Câu b :
-console.log('Nguyễn Lê Hoàng Long')
-console.clear()
+// // Bài 1 :
+// // Câu a :
+// console.log('Hello World')
+// // Câu b :
+// console.log('Nguyễn Lê Hoàng Long')
+// console.clear()
+// // Câu d :
+// console.log('*****')
+// console.log('*****')
+// console.log('*****')
+// console.log('*****')
+// console.log('*****')
+// // Câu e :
+// console.log('     *')
+// console.log('    ***')
+// console.log('   *****')
+// console.log('  *******')
+// console.log(' **********')
+// console.log('************')
+// // Câu f :
+// console.log(' ** ** ')
+// console.log('*  *  *')
+// console.log('*     *')
+// console.log(' *   *')
+// console.log('  * *')
+// console.log('   *')
+// console.clear()
+// // Bài 2
+// // Câu a
+// var a = 10
+// console.log(a)
+// // Câu b :
+// var b = '10'
+// console.log(b)
+// // Câu c :
+// var a = 20
+// console.log(a)
+// // Câu d :
+// const pi = 3.14
+// console.log(pi)
+// // Câu e :
+// if (true)
+// {
+// const pi = 5
+// console.log(pi)
+// }
+// console.clear()
+// // Câu f :
+// var dienthoai = {
+//     ten: 'iPhone 14', 
+//     hangSanXuat: 'Apple', 
+//     mauSac: 'Trang', 
+//     gia: '1000USD'
+// };
+// // Câu g :
+// dienthoai.gia = '700USD'
+// // Câu h : 
+// dienthoai.ten = 'iPhone 14 promax'
+// console.log(dienthoai)
+
+
+
+// // Bài 3
+// // Câu a :
+// let a = 1, b = 2
+// console.log('Tong =', a+b)
+// console.log('Hieu =', a-b)
+// console.log('Tich =', a*b)
+// console.log('Thuong =', a/b)
+// // Câu b :
+// console.log('Trung binh cong =', (a+b)/2 )
+// // Câu c :
+// let x = 3, y = 4, z = 6
+// console.log(x**2 + 2*x + 1)
+// console.log(x**3 - (3*x*y) - 5*y + 3*(y**2))
+// console.log((x*y)**2 - (2*(x**2)*y) + 13*y)
+// console.log(4*x**3 + 3*x*y + y**2 - (2*x*2 - 3*y))
+// console.log((5*x**2 / 4*x*y) + y**2)
+// console.log(x**2 - 2*z*x*y / y**2 + 5*x - 2*y**2 + 4*x*z**3 + z**3)
 // Câu d :
-console.log('*****')
-console.log('*****')
-console.log('*****')
-console.log('*****')
-console.log('*****')
-// Câu e :
-console.log('     *')
-console.log('    ***')
-console.log('   *****')
-console.log('  *******')
-console.log(' **********')
-console.log('************')
-// Câu f :
-console.log(' ** ** ')
-console.log('*  *  *')
-console.log('*     *')
-console.log(' *   *')
-console.log('  * *')
-console.log('   *')
-console.clear()
-// Bài 2
-// Câu a
-var a = 10
-console.log(a)
-// Câu b :
-var b = '10'
-console.log(b)
-// Câu c :
-const a = 20
-console.log(a)
+let d = 5 , pi = 3.14
+console.log('chu vi hinh tron R =', d*pi )
+let R = d*pi
+console.log(R)
+console.log('dien tich hinh tron S = ', pi*2*R)
