30b5c58f4ef876cbbadd68f02e8f51750dc1b4fd
JSbasic-Long <hoanglongngl98@gmai.com>
2023-09-25 19:42:08 +0700

    25/9/2023

--- .js
+++
@@ -1,9 +0,0 @@
-let hocSinh = {
-    hoTen: 'Nguyen Le Hoang Long',
-    tuoi: 25,
-    diaChi: 'so10duong1ckdc Vinh Loc'
-}   
-hocSinh.soDienThoai = 1235646789
-hocSinh.tuoi = 24 
-
-console.log(hocSinh)
---
+++ hamso.js
@@ -0,0 +1,7 @@
+function tinhTong(a,b){
+    const ketQua = a + b
+    return ketQua
+}
+const kq1 = tinhTong(1,2)
+console.log(kq1)
+function tinhHieu
