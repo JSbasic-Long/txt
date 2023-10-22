d287184dfbd60e0d368506bebbbbcb2d0205cbe9
JSbasic-Long <hoanglongngl98@gmai.com>
2023-10-22 13:28:36 +0700

    a

---
+++ C.js
@@ -0,0 +1,98 @@
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     cout << "Nhập 2 số: ";
+//     int x,y;
+//     cin >> x >> y;
+//     cout << "Tổng bình phương 2 số = " << (x*x+y*y); 
+// }
+
+// int main(){
+//     cout << "Xin chào" << endl << "Tôi tên là LCS";
+// }
+
+// int main(){
+//     cout<<"Nhập bán kính: ";
+//     int x;
+//     cin >> x;
+//     const float PI = 3.1415;
+//     int c = (2*PI*x);
+//     cout<<"Chu vi đường tròn: "<< c << endl;
+//     cout<<"Diện tích đường tròn: "<<(PI*x*x);
+// }
+
+// Nhập số lượng sản phẩm và đơn giá của 1 sản phẩm. Xuất ra thành tiền, VAT 10% và tổng tiền phải trả.
+
+// int main(){
+//     cout<< "Nhập số lượng; ";
+//     int x;
+//     cin >> x;
+//     cout<< "Nhập đơn giá: ";
+//     int y;
+//     cin >> y;
+//     int thanhTien = x*y;
+//     cout<< "Thành tiền; "<< thanhTien<<endl;
+//     int VAT = thanhTien * 10 / 100;
+//     cout<< "VAT: "<< VAT<<endl;
+//     cout<<"Tổng tiền: "<< thanhTien + VAT;
+// }
+
+// 5. Nhập số nguyên có 2 chữ số. Xuất ra số đảo ngược của số đó (ví dụ: Nhập 74, xuất 47).
+
+
+// int main(){
+//     cout<< "Nhập số cần đảo ngược: ";
+//     int x;
+//     cin >> x;
+//     int hangChuc = x / 10;
+//     int hangDonVi = x % 10;
+//     int tong =  hangDonVi * 10 + hangChuc; 
+//     cout <<"Số đảo ngược:  "<< tong;
+// }
+
+
+
+//Nhập số nguyên có 3 chữ số. Xuất ra tổng 3 chữ số của số đó.
+// int main(){
+//     cout<<"Nhập số nguyên 3 chữ số: ";
+//     int x;
+//     cin >> x;
+//     int hangTram = x / 100 ;
+//     int so2 = x % 100;
+//     int hangChuc = so2 / 10 ;
+//     int hangDonvi = so2 % 10 ;
+//     cout<<"Tổng 3 chữ số: "<< hangTram + hangChuc + hangDonvi;
+// }
+
+
+//7. Nhập vào tổng số giây. Quy đổi thành giờ, phút, giây và xuất ra màn hình.
+
+// int main(){
+//     cout<< "Nhập tổng số giây: ";
+//     int x;
+//     cin >> x;
+//     int gio = x / 3600;
+//     int phut = (x - (gio * 3600)) / 60 ;
+//     int giay = (x - (gio * 3600)) % 60 ;
+//     cout << gio<<" Giờ " <<endl<< phut<<" Phút "<<endl<<giay<<" Giây ";
+// }
+
+
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
